47da6185c694728b2e4ca655d87067f85a4ab177
huthaifadl <huthaifasyr7@gmail.com>
2025-05-24 18:02:02 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const quotes = [
+  {
+    author: "Muhammad Ali",
+    quoteText:
+      "“When a man says I cannot, he has made a suggestion to himself. He has weakened his power of accomplishing that which otherwise would have been accomplished.”",
+  },
+  {
+    author: "The Prophet Muhammad ﷺ",
+    quoteText:
+      "“Do good deeds properly, sincerely and moderately and know that your deeds will not make you enter Paradise, and that the most beloved deed to Allah is the most regular and constant even if it were little.”",
+  },
+  {
+    author: "Nouman Ali Khan",
+    quoteText:
+      "“It is when things are at their worst that Allah will raise the best generation. The generation that the Prophet would be told Sahabat should look up to. So maybe the fact that you are living in the darkest of time means that Allah thinks you can be the strongest source of light.”",
+  },
+  {
+    author: "Abu Hamid al-Ghazali",
+    quoteText:
+      "“a man should say to his soul every morning, God has given thee twenty-four treasures; take heed lest thou lose anyone of them, for thou wilt not be able to endure the regret that will follow such loss”",
+  },
+  {
+    author: "Nouman Ali Khan",
+    quoteText:
+      "“What you desire is what you want, what you have is what Allah wants”",
+  },
+];
+
+function changeQuote () {
+    let randomQuote = quotes[Math.floor(quotes.length * Math.random())];
+    document.getElementById("quoteTextP").innerHTML = randomQuote.quoteText;
+    document.getElementById("quoteAuthor").innerHTML = `— ${randomQuote.author}`;
+}
